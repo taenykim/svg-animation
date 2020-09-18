17dfba08dcda0626b26464f819d22972584deb5f
taeeun kim <kimtaeeeny@gmail.com>
2020-09-19 01:05:14 +0900

    .

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-/**
- * 로딩 UI
- */
+const generateRandomColor = (base) =>
+  Array(6)
+    .fill()
+    .reduce((acc, _) => acc + base[Math.floor(Math.random() * base.length)], "#")
+
 window.onload = async () => {
-  const queue = ["#ffffff", "#ff00ff", "ff0000"]
   const logo = document.querySelector("#logo")
-  const logo2 = document.querySelector("#logo2")
   const path = document.querySelector("#path1")
   const totalLength = path.getTotalLength()
 
@@ -17,6 +17,7 @@ window.onload = async () => {
   path.style[`stroke-dashoffset`] = totalLength
 
   let time = 0
+  let color
 
   let frame = () => {
     const currentLength = path.getPointAtLength(time)
@@ -24,26 +25,18 @@ window.onload = async () => {
     const { x: nextX, y: nextY } = path.getPointAtLength(time)
     const { x: prevX, y: prevY } = path.getPointAtLength(time - 2)
     const rad = Math.atan2(nextY - prevY, nextX - prevX) + (Math.PI * 2) / 4
-    logo.style.cssText = `
+    logo.style.cssText = `fill: ${color};
     transform: translate(${currentLength.x - LOGO_WIDTH}px, ${currentLength.y - LOGO_HEIGHT}px) rotate(${rad}rad)`
     if (totalLength - time <= (totalLength / 100) * 0 + LENGTH_ALLOW_RANGE) {
       time = 0
+      color = generateRandomColor("0123456789abc")
+      path.style.stroke = color
       requestAnimationFrame(frame)
       return
-    } else if (
-      totalLength - time <= (totalLength / 100) * 65 + LENGTH_ALLOW_RANGE &&
-      totalLength - time >= (totalLength / 100) * 35 + LENGTH_ALLOW_RANGE
-    ) {
-      time += 10
-      requestAnimationFrame(frame)
-    } else if (totalLength - time <= (totalLength / 100) * 25 + LENGTH_ALLOW_RANGE) {
-      time += 10
-      requestAnimationFrame(frame)
     } else {
       time += 10
       requestAnimationFrame(frame)
     }
   }
-
   requestAnimationFrame(frame)
 }
